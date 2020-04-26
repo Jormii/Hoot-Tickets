1253bc2478629144a818828fd05ab8a1b58222f8
Yolanda <yolanda.gomez.h@gmail.com>
2020-04-26 17:55:17 +0200

    Update HootTicketsApplication.java
    import necesarios

--- HootTickets/src/main/java/dad/hoottickets/HootTicketsApplication.java
+++ HootTickets/src/main/java/dad/hoottickets/HootTicketsApplication.java
@@ -2,7 +2,14 @@ package dad.hoottickets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+
+@EnableHazelcastHttpSession
 @SpringBootApplication
 public class HootTicketsApplication {
 
